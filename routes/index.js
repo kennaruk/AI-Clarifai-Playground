7c25393516fbe5fb874e0575efc49f34c8901933
Ken Nitipat <k-e-n_ken@hotmail.com>
2017-12-03 22:15:33 +0700

    file transfer to backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
-
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.ejs');
+});
+
+router.post('/upload', function(req, res, next) {
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+  console.log('files found.');
+
+  let fileUploaded = req.files;
+  console.log(fileUploaded['images[]']);
+
+  res.status(200).send('file found.');
+
 });
 
 module.exports = router;
